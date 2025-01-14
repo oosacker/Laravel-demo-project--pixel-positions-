c501e4f6fa1b050c7351511ff5183879b029d211
Natsuki Hasegawa <54010582+oosacker@users.noreply.github.com>
2025-01-14 14:13:59 +0800

    Layouts wp

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,22 +6,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Pixel Positions</title>
-  @Vite('resources/js/app.js')
+  @Vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 
-<body>
-  <div class="">
-    <nav>
-      <div>
+<body class="bg-black text-white">
+  <div class="px-10">
+    <nav class="flex justify-between items-center py-4 border-b border-white/10">
+      <div class="flex items-center">
         <a href="">
           <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Logo">
         </a>
       </div>
-      <div>Links</div>
+
+      <div class="space-x-6 font-bold">
+        <a class="" href="">Jobs</a>
+        <a class="" href="">Careers</a>
+        <a class="" href="">Salaries</a>
+        <a class="" href="">Companies</a>
+      </div>
+
       <div>Post a Job</div>
     </nav>
 
-    <main>{{ $slot }}</main>
+    <main class="mt-10 max-w-[986px]">
+      {{ $slot }}
+    </main>
   </div>
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,25 @@
-<x-layout></x-layout>
+<x-layout>
+  <section>
+    <h3 class="mb-6">Top Jobs</h3>
+
+    <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+      <div class="self-start text-sm">Laracasts</div>
+
+      <div>
+        <h3>Video Producer</h3>
+        <p>Full time - from $60,000</p>
+      </div>
+
+      <div class="flex justify-between items-center mt-auto">
+        <div>
+          <a href="bg-white/10 px-2 py-12">Tag</a>
+          <a href="">Tag</a>
+          <a href="">Tag</a>
+        </div>
+
+        <img src="https://placehold.co/42" alt="">
+      </div>
+
+    </div>
+  </section>
+</x-layout>
